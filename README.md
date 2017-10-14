85b537213b85d848df6ed37fcc53173c2fd1bec2
jeff <jeff@jamma.cn>
2017-10-14 19:52:40 +0800

    支持账号密码方式登陆

--- README.md
+++ README.md
@@ -7,7 +7,3 @@ general passport service
 基本配置 请参考 [jm-server] (https://github.com/jm-root/jm-server)
 
 gateway Gateway服务器Uri
-
-disablePassportMobile 是否禁止 jm-passport-mobile 插件, 默认不禁止
-
-disablePassportMongodb 是否禁止 jm-passport-mongodb 插件, 默认不禁止
--- config/index.js
+++ config/index.js
@@ -1,42 +1,30 @@
-require('log4js').configure(__dirname + '/log4js.json');
+require('log4js').configure(require('path').join(__dirname, 'log4js.json'))
 var config = {
-    development: {
-        debug: true,
-        lng: 'zh_CN',
-        port: 3000,
-        gateway: 'http://api.mx.jamma.cn',
-        modules: {
-            'passport': {
-                module: process.cwd() + '/lib'
-            },
-            'jm-passport-mobile': {},
-            'jm-passport-mongodb': {}
-        }
-    },
-    production: {
-		lng: 'zh_CN',
-        port: 3000,
-        gateway: 'http://api.mx.jamma.cn',
-        redis: 'redis://redis.db',
-        db: 'mongodb://mongo.db/passport',
-        tokenExpire: 3600,
-        disableVerifyCode: false,
-        disableAutoUid: false,
-        modules: {
-            'passport': {
-                module: process.cwd() + '/lib'
-            },
-            'jm-passport-mobile': {},
-            'jm-passport-mongodb': {}
-        }
+  development: {
+    debug: true,
+    lng: 'zh_CN',
+    port: 3000,
+    gateway: 'http://localhost:3001',
+    modules: {
+      'passport': {
+        module: process.cwd() + '/lib'
+      }
     }
-};
+  },
+  production: {
+    lng: 'zh_CN',
+    port: 80,
+    gateway: 'http://gateway.app',
+    modules: {
+      'passport': {
+        module: process.cwd() + '/lib'
+      }
+    }
+  }
+}
 
-var env = process.env.NODE_ENV || 'development';
-config = config[env] || config['development'];
-config.env = env;
+var env = process.env.NODE_ENV || 'development'
+config = config[env] || config['development']
+config.env = env
 
-if(process.env['disablePassportMobile']) delete config.modules['jm-passport-mobile'];
-if(process.env['disablePassportMongodb']) delete config.modules['jm-passport-mongodb'];
-
-module.exports = config;
+module.exports = config
